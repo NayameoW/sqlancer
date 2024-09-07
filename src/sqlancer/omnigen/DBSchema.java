7ceb30a8a82283a9b87ad0c93afeb39df025ff5d
NayameoW <naya1207445468@foxmail.com>
2024-09-07 15:08:02 +0800

    Update DBSchema.java

--- src/sqlancer/omnigen/DBSchema.java
+++ src/sqlancer/omnigen/DBSchema.java
@@ -1,6 +1,11 @@
 package sqlancer.omnigen;
 
+import sqlancer.common.schema.AbstractRelationalTable;
 import sqlancer.common.schema.AbstractSchema;
+
+import sqlancer.common.schema.AbstractTableColumn;
+import sqlancer.common.schema.TableIndex;
+import sqlancer.omnigen.DBSchema.DBTable;
 
 import java.util.List;
 
@@ -8,4 +13,30 @@ public class DBSchema extends AbstractSchema<DBGlobalState, DBTable> {
     public DBSchema(List<DBTable> databaseTables) {
         super(databaseTables);
     }
+
+    public static class DBTable extends AbstractRelationalTable<DBColumn, DBIndex, DBGlobalState> {
+
+        public DBTable(String name, List<DBColumn> columns, List<DBIndex> indexes, boolean isView) {
+            super(name, columns, indexes, isView);
+        }
+    }
+
+    public static class DBColumn extends AbstractTableColumn<DBTable, DBDataType> {
+
+        public DBColumn(String name, DBTable table, DBDataType type) {
+            super(name, table, type);
+        }
+    }
+
+    public enum DBDataType{
+        INT;
+    }
+
+    public static final class DBIndex extends TableIndex{
+
+        private DBIndex(String indexName) {
+            super(indexName);
+        }
+    }
+
 }
